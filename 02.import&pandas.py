8aecfe3d7e2ef2d141c9edd9c5242fe3a8255efa
jwryu87 <jwryu87@gmail.com>
2020-09-01 10:40:55 +0900

    title: data import & pandas & test data

--- 02.import&pandas.py
+++ 02.import&pandas.py
@@ -42,14 +42,38 @@ if __name__ == "__main__":
 
 
 # (3) test data 만들기
+# 3-1 하지만 이것은 다시 실행하면 다른 테스트 세트가 되어 버린다.
 import numpy as np
 
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
+# def split_train_test(data, test_ratio):
+#     shuffled_indices = np.random.permutation(len(data))
+#     test_set_size = int(len(data) * test_ratio)
+#     test_indices = shuffled_indices[:test_set_size]
+#     train_indices = shuffled_indices[test_set_size:]
+#     return data.iloc[train_indices], data.iloc[test_indices]
+#
+# train_set, test_set = split_train_test(housing, 0.2)
+# print(len(train_set), "train +", len(test_set), "test")
 
-train_set, test_set = split_train_test(housing, 0.2)
-print(len(train_set), "train +", len(test_set), "test")
+# 3-2 해결책으로 샘플의 식별자를 사용하여 테스트 세트로 보낼지 말지를 정하는것
+# - 예를 들어 각 샘플마다 식별잘의 해시값을 계산하여 해시의 마지막 바이트의 값이 51(256의 20% 정도)보다 작거나 같은 샘플만 테스트 세트로 보낼 수 있습니다.
+
+from  zlib import crc32
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_:test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+# 주택 데이터 셋에는 식별자 컬럼이 없으므로 대신 행의 인덱스를 ID로 사용
+# housing_with_id = housing.reset_index() # index 열이 추가된 데이터 프레임을 반환합니다.
+# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+# 3-3 사이킷런은 데이터셋을 여러 서브셋으로 나누는 다양한 방법을 제공합니다.
+# - train_test_split
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
